9d903f619c3d96ea439fa42c14a74429c393234d
Mustafa <mustafaj1919@gmail.com>
2026-06-06 15:15:04 +0300

    fix: use relative paths for launch and autostart scripts

--- setup_autostart.bat
+++ setup_autostart.bat
@@ -1,13 +1,12 @@
 @echo off
 set STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
-set VBS=%~dp0autostart.vbs
+set PROJECT=%~dp0
 
-copy /Y "%VBS%" "%STARTUP%\CarShowroom.vbs" >nul
+echo Set oShell = CreateObject("WScript.Shell") > "%STARTUP%\CarShowroom.vbs"
+echo oShell.Run """"%PROJECT%launch.bat"""", 0, False >> "%STARTUP%\CarShowroom.vbs"
 
 echo.
 echo Done! App will start automatically on every Windows login.
-echo.
-echo To remove autostart, delete this file:
-echo %STARTUP%\CarShowroom.vbs
+echo Path: %PROJECT%
 echo.
 pause
